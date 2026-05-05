1451ae88f8691722a902d9abb523e1266cd72b86
Marcelo Garcia <marcelogarciarojas@gmail.com>
2026-05-04 21:15:49 -0400

    Fix content security policy

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,13 +5,14 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
   this.timeout(10000);
 
-  test('Viewing one stock: GET request to /api/stock-prices/', function(done) {
-    chai.request(server)
+  test('Viewing one stock: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
       .get('/api/stock-prices?stock=GOOG')
-      .end(function(err, res) {
+      .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.isObject(res.body.stockData);
         assert.isString(res.body.stockData.stock);
@@ -21,56 +22,79 @@ suite('Functional Tests', function() {
       });
   });
 
-  test('Viewing one stock and liking it: GET request to /api/stock-prices/', function(done) {
-    chai.request(server)
+  test('Viewing one stock and liking it: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
       .get('/api/stock-prices?stock=AAPL&like=true')
-      .end(function(err, res) {
-        assert.equal(res.status, 200);
-        assert.isObject(res.body.stockData);
-        assert.equal(res.body.stockData.stock, 'AAPL');
-        assert.isNumber(res.body.stockData.price);
-        assert.isAtLeast(res.body.stockData.likes, 1);
-        done();
-      });
-  });
-
-  test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function(done) {
-    chai.request(server)
-      .get('/api/stock-prices?stock=AAPL&like=true')
-      .end(function(err, res) {
+      .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.isObject(res.body.stockData);
         assert.equal(res.body.stockData.stock, 'AAPL');
         assert.isNumber(res.body.stockData.price);
         assert.isNumber(res.body.stockData.likes);
+        assert.isAtLeast(res.body.stockData.likes, 1);
         done();
       });
   });
 
-  test('Viewing two stocks: GET request to /api/stock-prices/', function(done) {
-    chai.request(server)
+  test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=NFLX&like=true')
+      .end(function (err, firstRes) {
+        assert.equal(firstRes.status, 200);
+
+        const firstLikes = firstRes.body.stockData.likes;
+
+        chai
+          .request(server)
+          .get('/api/stock-prices?stock=NFLX&like=true')
+          .end(function (err, secondRes) {
+            assert.equal(secondRes.status, 200);
+            assert.equal(secondRes.body.stockData.likes, firstLikes);
+            done();
+          });
+      });
+  });
+
+  test('Viewing two stocks: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
       .get('/api/stock-prices?stock=GOOG&stock=MSFT')
-      .end(function(err, res) {
+      .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.isArray(res.body.stockData);
         assert.lengthOf(res.body.stockData, 2);
-        assert.property(res.body.stockData[0], 'rel_likes');
-        assert.property(res.body.stockData[1], 'rel_likes');
-        assert.notProperty(res.body.stockData[0], 'likes');
-        assert.notProperty(res.body.stockData[1], 'likes');
+
+        assert.isString(res.body.stockData[0].stock);
+        assert.isNumber(res.body.stockData[0].price);
+        assert.isNumber(res.body.stockData[0].rel_likes);
+
+        assert.isString(res.body.stockData[1].stock);
+        assert.isNumber(res.body.stockData[1].price);
+        assert.isNumber(res.body.stockData[1].rel_likes);
+
         done();
       });
   });
 
-  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function(done) {
-    chai.request(server)
+  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
       .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
-      .end(function(err, res) {
+      .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.isArray(res.body.stockData);
         assert.lengthOf(res.body.stockData, 2);
+
+        assert.isString(res.body.stockData[0].stock);
+        assert.isNumber(res.body.stockData[0].price);
         assert.isNumber(res.body.stockData[0].rel_likes);
+
+        assert.isString(res.body.stockData[1].stock);
+        assert.isNumber(res.body.stockData[1].price);
         assert.isNumber(res.body.stockData[1].rel_likes);
+
         done();
       });
   });
